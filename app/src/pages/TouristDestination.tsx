75d3fb9cd1ae90e7aa1e151681d5f98a652b3e27
stormragemc <stormragemc2005@gmail.com>
2024-07-16 14:31:54 +0700

    Update TouristDestination.tsx

--- app/src/pages/TouristDestination.tsx
+++ app/src/pages/TouristDestination.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { TouristDestinations } from '../components/TouristDestinations/TouristDestinations'
-import { Slideshow } from '../components/TouristDestinations/Slide test'
+
 
 const TouristDestination = () => {
   return (
