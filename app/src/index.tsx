a79d150f305b8f9029699654db6bb48b2a8bf2b5
stormragemc <stormragemc2005@gmail.com>
2024-07-16 14:33:09 +0700

    added tourist destination page to navigation

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,8 +4,9 @@ import { HashRouter, Route, Routes } from 'react-router-dom';
 // import './style/header.css';
 import Home from './pages/Home';
 import Test from './pages/Test';
-import CreateList from './components/reservationList/ReservationLists';
+import ResList from './pages/Reservation';
 import Layout from './Layout';
+import TouristDestination from './pages/TouristDestination';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -17,7 +18,8 @@ root.render(
         <Routes>
           <Route path="/test" element={<Test />} />
           <Route path="/" element={<Home />} />
-          <Route path="/ResL" element={<CreateList />} />
+          <Route path="/ResL" element={<ResList />} />
+          <Route path = "/Destinations" element = {<TouristDestination/>}/>
         </Routes>
       </Layout>
     </HashRouter>
