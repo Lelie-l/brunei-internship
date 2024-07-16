1f5722c137ef91f931d92e7429a7a288cdab20e8
stormragemc <stormragemc2005@gmail.com>
2024-07-16 14:31:12 +0700

    Add files via upload

---
+++ app/src/pages/TouristDestination.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { TouristDestinations } from '../components/TouristDestinations/TouristDestinations'
+import { Slideshow } from '../components/TouristDestinations/Slide test'
+
+const TouristDestination = () => {
+  return (
+    <div className = "classreservation column" id = "mainreservation">
+     <TouristDestinations/>
+    </div>
+  )
+}
+
+export default TouristDestination
