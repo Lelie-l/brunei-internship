4534683cdc9777521f1d2320ee5d1eea55e76e85
Kaiden <brianbatara.yaputra@gmail.com>
2024-07-16 16:09:01 +0700

    fixed overlapping issue and added header feature
    made header disappear after 500px scrolling

--- app/src/Layout.tsx
+++ app/src/Layout.tsx
@@ -4,31 +4,37 @@ import React, { ReactNode } from 'react';
 
 interface LayoutProps {
   children: ReactNode;
-} //fail maybe?
+}
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    const header = document.getElementById("header");
+    if (header) {
+      if (currentScrollPos > 500) {
+        header.style.top = "-50px";
+      } else {
+        header.style.top = "0px";
+      }
+    }
+  }
+
   return (
-    // <div id="home-header-container">
-    //   <img alt="ic_home_top" src={require(".s/../assets/img/banner.png")} />
-    //   <div className="info-card user-info-card">
-    //     <span>Welcome to EMAS.</span>
-    //   </div>
-    // </div>
-    // <div className="backgroundimage">
     <div>
-    <header className="header">
-        <div className="logo">
-            <h1>Br<span className="white">u</span><span className="black">n</span>ei<span className="white">.</span></h1>
-        </div>
-        <ul className="stuff">
-            <li className="item"><Link to="/ResL">Reservation</Link></li>
-            <li className="item"><Link to = "/Destinations">Things To Do</Link></li>
-            <li className="item"><a href="#about">About Brunei</a></li>
-        </ul>
-    </header>
-    <main>
-        {children}
-    </main>
+      <header id="header" className="header">
+          <div className="logo">
+              <h1>Br<span className="white">u</span><span className="black">n</span>ei<span className="white">.</span></h1>
+          </div>
+          <ul className="stuff">
+              <li className="item"><Link to="/ResL">Reservation</Link></li>
+              <li className="item"><Link to="/Destinations">Things To Do</Link></li>
+              <li className="item"><a href="#about">About Brunei</a></li>
+          </ul>
+      </header>
+      <main>
+          {children}
+      </main>
     </div>
   );
 }
