0822b1e54759828a9fe1f91577849bcda502c153
Kaiden <brianbatara.yaputra@gmail.com>
2024-07-16 14:21:23 +0700

    Update index.tsx

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -19,10 +19,7 @@ root.render(
           <Route path="/test" element={<Test />} />
           <Route path="/" element={<Home />} />
           <Route path="/ResL" element={<ResList />} />
-<<<<<<< HEAD
-=======
           <Route path = "/Destinations" element = {<TouristDestination/>}/>
->>>>>>> a79d150f305b8f9029699654db6bb48b2a8bf2b5
         </Routes>
       </Layout>
     </HashRouter>
