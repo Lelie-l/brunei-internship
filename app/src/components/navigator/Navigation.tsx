47409fabe72dd770003f77d8e5e68088cf824546
stormragemc <stormragemc2005@gmail.com>
2024-07-22 11:54:31 +0700

    Update Navigation.tsx

--- app/src/components/navigator/Navigation.tsx
+++ app/src/components/navigator/Navigation.tsx
@@ -3,21 +3,24 @@ import '../../style/header.css';
 import hotelimg from '../../assets/img/bed.png';
 import monumentimg from '../../assets/img/monument.png';
 import planeimg from '../../assets/img/plane.png'
-import { scrollToDiv } from "../../utils/scrolltoDiv";
+import { scrollToDiv, PopUpForm } from "../../utils/scrolltoDiv";
 
 const CreateNavigation: React.FC = () => {
-
+    
     
     return (
         <div className="buttonNavigation">
+      
+
             <a onClick={() => scrollToDiv('')}>
                 <img src={monumentimg} alt="hotel" height="18px" /><p>Attractions</p>
             </a>
             <a onClick={() => scrollToDiv('mainreservation')}>
                 <img src={hotelimg} alt="hotel" height="18px" /><p>Accommodations</p>
             </a>
-            <a onClick={() => scrollToDiv('mainreservation')}>
+            <a className = "flight">
             <img src={planeimg} alt="hotel" height="18px" /><p>Flights</p>
+            <PopUpForm/>
             </a>
         </div>
     );
