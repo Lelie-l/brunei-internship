ad9d3790d70af914d53eedb57e03dcd36798febb
Kaiden <brianbatara.yaputra@gmail.com>
2024-07-22 15:35:06 +0700

    fixed issue where navbar was too close to page contents

--- app/src/pages/BeautifulCities.tsx
+++ app/src/pages/BeautifulCities.tsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import { BeautifulCitiesC } from '../components/BeautifulCitiesC'
-import CreateNavigation from '../components/navigator/Navigation'
+import React from "react";
+import { BeautifulCitiesC } from "../components/BeautifulCitiesC";
+import CreateNavigation from "../components/navigator/Navigation";
 const BeautifulCities = () => {
   return (
     <div>
-        <div className = "bodytext" style = {{alignItems:'center',backgroundImage: 'url(https://images.squarespace-cdn.com/content/v1/5e72c8bfe21ad940ba788673/1621088501890-G9PLJ8QNZCSX4JSVTK4D/kampong-ayer-top-image.jpg)'}}>
-        <h1 style = {{fontSize:'40px'}}>Beautiful Cities</h1>
-        </div>
-        <CreateNavigation/>
+      <div
+        className="bodytext"
+        style={{
+          alignItems: "center",
+          backgroundImage:
+            "url(https://images.squarespace-cdn.com/content/v1/5e72c8bfe21ad940ba788673/1621088501890-G9PLJ8QNZCSX4JSVTK4D/kampong-ayer-top-image.jpg)",
+        }}
+      >
+        <h1 style={{ fontSize: "40px" }}>Beautiful Cities</h1>
+      </div>
 
-    <div className="formatContent">
-      <BeautifulCitiesC/>
-    </div>
-    </div>
-  )
-}
+      <div style={{marginBottom: "2rem"}}>
+        <CreateNavigation />
+      </div>
 
-export default BeautifulCities
+      <div className="formatContent">
+        <BeautifulCitiesC />
+      </div>
+    </div>
+  );
+};
+
+export default BeautifulCities;
--- app/src/pages/Nature.tsx
+++ app/src/pages/Nature.tsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import { NatureC } from '../components/NatureC'
-import CreateNavigation from '../components/navigator/Navigation'
+import React from "react";
+import { NatureC } from "../components/NatureC";
+import CreateNavigation from "../components/navigator/Navigation";
 const Nature = () => {
   return (
     <div>
-    <div className = "bodytext" style = {{alignItems:'center',backgroundImage: 'url(https://i0.wp.com/borneoadventure.com/v3/wp-content/uploads/2012/01/BRN_DK_5221.jpg?fit=2000%2C1331&quality=60&strip=all&ssl=1)' }}>
-    <h1 style = {{fontSize:'40px'}}>Nature</h1>
-    </div>
-    
-    <CreateNavigation/>
+      <div
+        className="bodytext"
+        style={{
+          alignItems: "center",
+          backgroundImage:
+            "url(https://i0.wp.com/borneoadventure.com/v3/wp-content/uploads/2012/01/BRN_DK_5221.jpg?fit=2000%2C1331&quality=60&strip=all&ssl=1)",
+        }}
+      >
+        <h1 style={{ fontSize: "40px" }}>Nature</h1>
+      </div>
 
-    <div className="formatContent">
-      <NatureC/>
-    </div>
-    
-    </div>
-  )
-}
+      <div style={{ marginBottom: "2rem" }}>
+        <CreateNavigation />
+      </div>
 
-export default Nature
+      <div className="formatContent">
+        <NatureC />
+      </div>
+    </div>
+  );
+};
+
+export default Nature;
